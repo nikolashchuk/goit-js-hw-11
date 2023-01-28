77cce3caba51ed85363fa04e08c717f9f98ccd3b
nikolashchuk <nikolashchuk@gmail.com>
2023-01-28 13:52:36 +0200

    add 11

--- src/index.js
+++ src/index.js
@@ -1,76 +1,101 @@
-import './css/styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import debounce from 'lodash.debounce';
-import { fetchCountries } from './js/fetchCountries';
+import fetchPhotos from './js/fetchPhotos';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const DEBOUNCE_DELAY = 300;
+let query = '';
+let page;
+let newpages;
 
-const input = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
+const target = document.querySelector('.target');
+const options = {
+  root: null,
+  rootMargin: '300px',
+  threshold: 1.0,
+};
+const observer = new IntersectionObserver(updatePhotos, options);
+//
+const searchForm = document.querySelector('#search-form');
+const galleryBox = document.querySelector('.gallery');
 
-input.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+searchForm.addEventListener('submit', onFormSubmit);
 
-function onInputSearch(evt) {
-  if (!evt.target.value) {
-    resetInput();
-    Notiflix.Notify.warning('Please enter country name');
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  galleryBox.innerHTML = '';
+  observer.unobserve(target);
+  page = 1;
+  query = e.target.elements.searchQuery.value;
+  if (!query) {
+    Notiflix.Notify.failure('Enter any value');
     return;
   }
-  return fetchCountries(evt.target.value.trim())
-    .then(showCountry)
-    .catch(showError);
+  fetchPhotos(query, page).then(response => {
+    console.log(response.data.hits);
+    if (response.data.totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    //
+    if (response.data.totalHits > 1) {
+      Notiflix.Notify.success(
+        `Hooray! We found ${response.data.totalHits} images.`
+      );
+    }
+
+    renderCards(response.data.hits);
+    observer.observe(target);
+  });
 }
 
-function showCountry(countries) {
-  if (countries.length > 10) {
-    resetInput();
-    Notiflix.Notify.info(
-      'Too many matches found. Please enter a more specific name.'
-    );
-    return;
-  }
-  if (countries.length > 1 && countries.length <= 10) {
-    resetInput();
-    countryList.innerHTML = countryListMarkup(countries);
-    return;
-  }
-  resetInput();
-  countryInfo.innerHTML = countryInfoMarkup(countries);
-}
-
-function countryInfoMarkup(countries) {
-  return countries
-    .map(({ flags, name, capital, population, languages }) => {
-      return `<img src="${flags.svg}" alt="${
-        name.official
-      }" width="60" height="40" />
-        <h1>${name.official}</h1>
-        <p>Capital: ${capital}</p>
-        <p>Population: ${population}</p>
-        <p>Languages: ${Object.values(languages)}</p>`;
+function renderCards(images) {
+  const listOfPhotos = images
+    .map(image => {
+      return `<div class="photo-card">
+  <a class="card-link" href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${image.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${image.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${image.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${image.downloads}</b>
+    </p>  
+  </div>
+</div>`;
     })
     .join('');
+
+  galleryBox.insertAdjacentHTML('beforeend', listOfPhotos);
+
+  const lightbox = new SimpleLightbox('.gallery a', {});
+  lightbox.refresh();
 }
 
-function countryListMarkup(countries) {
-  return countries
-    .map(({ flags, name }) => {
-      return `<li>
-        <img src="${flags.svg}" alt="${name.official}" width="50" height="30" />
-        <p>${name.official}</p>
-        </li>`;
-    })
-    .join('');
-}
-
-function showError() {
-  resetInput();
-  Notiflix.Notify.failure('Oops, there is no country with that name');
-}
-
-function resetInput() {
-  countryList.innerHTML = '';
-  countryInfo.innerHTML = '';
+function updatePhotos(entries) {
+  console.log(entries);
+  newpages = entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      console.log(query);
+      page += 1;
+      fetchPhotos(query, page).then(response => {
+        if (response.data.totalHits < page * 40) {
+          Notiflix.Notify.failure(
+            `We're sorry, but you've reached the end of search results.`
+          );
+          observer.unobserve(target);
+          return;
+        }
+        renderCards(response.data.hits);
+      });
+    }
+  });
 }
